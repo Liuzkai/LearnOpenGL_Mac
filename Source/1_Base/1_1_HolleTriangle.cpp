75dfae582dbdcd78252fbef88c39e9dc1220038d
LiuzKai <liuzkai2012@gmail.com>
2022-05-28 13:23:00 +0800

    1.1HolleTriangle

---
+++ Source/1_Base/1_1_HolleTriangle.cpp
@@ -0,0 +1,14 @@
+#include <glad.h>
+#include <glfw3.h>
+
+
+int main()
+{
+    glfwInit();
+    // 设置OpenGL版本信息
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    return 0;
+}
