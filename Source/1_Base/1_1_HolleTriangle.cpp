a15967ee74762054dca1b5d920de7fa2ae5dc7e6
LiuzKai <liuzkai2012@gmail.com>
2022-05-28 13:37:31 +0800

    1.1 base knowledge for glfw

--- Source/1_Base/1_1_HolleTriangle.cpp
+++ Source/1_Base/1_1_HolleTriangle.cpp
@@ -1,14 +1,24 @@
 #include <glad.h>
 #include <glfw3.h>
+#include <iostream>
 
 
 int main()
 {
     glfwInit();
-    // 设置OpenGL版本信息
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // 设置OpenGL版本信息,因为我不知道Mac上的版本信息，干脆就别设置了。
+//    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+//    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+//    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if(window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
 
     return 0;
 }
